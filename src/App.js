4a8ee8a7391b65dbdc9f0cae4f81d918c501da71
Ninjavin <vinjain2006@gmail.com>
2020-11-29 23:30:06 +0530

    Added React Hooks

--- src/App.js
+++ src/App.js
@@ -1,52 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import CharPicker from './components/CharPicker';
 import Character from './components/Character';
 
-class App extends Component {
-  state = {
-    selectedCharacter: 1,
-    side: 'light',
-    destroyed: false
+const App = props => {
+  const [side, setSide] = useState('light')
+  const [char, setChar] = useState(1)
+  const [destruction, setDestruction] = useState(false)
+
+  const sideHandler = side => {
+    setSide(side);
   };
 
-  sideHandler = side => {
-    this.setState({ side: side });
-  };
-
-  charSelectHandler = event => {
+  const charSelectHandler = event => {
     const charId = event.target.value;
-    this.setState({ selectedCharacter: charId });
+    setChar(charId);
   };
 
-  destructionHandler = () => {
-    this.setState({ destroyed: true });
+  const destructionHandler = () => {
+    setDestruction(true)
   };
+  
+  let content = (
+    <React.Fragment>
+      <CharPicker
+        side={side}
+        selectedChar={char}
+        onCharSelect={charSelectHandler}
+      />
+      <Character selectedChar={char} />
+      <button onClick={sideHandler.bind(this, 'light')}>Light Side</button>
+      <button onClick={sideHandler.bind(this, 'dark')}>Dark Side</button>
+      {side === 'dark' && (
+        <button onClick={destructionHandler}>DESTROY!</button>
+      )}
+    </React.Fragment>
+  );
 
-  render() {
-    let content = (
-      <React.Fragment>
-        <CharPicker
-          side={this.state.side}
-          selectedChar={this.state.selectedCharacter}
-          onCharSelect={this.charSelectHandler}
-        />
-        <Character selectedChar={this.state.selectedCharacter} />
-        <button onClick={this.sideHandler.bind(this, 'light')}>
-          Light Side
-        </button>
-        <button onClick={this.sideHandler.bind(this, 'dark')}>Dark Side</button>
-        {this.state.side === 'dark' && (
-          <button onClick={this.destructionHandler}>DESTROY!</button>
-        )}
-      </React.Fragment>
-    );
-
-    if (this.state.destroyed) {
-      content = <h1>Total destruction!</h1>;
-    }
-    return content;
+  if (destruction) {
+    content = <h1>Total destruction!</h1>;
   }
-}
+  return content;
+};
 
 export default App;
--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,63 +1,50 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './CharPicker.css';
 
-class CharPicker extends Component {
-  state = { characters: [], isLoading: false };
+const CharPicker = props => {
+  const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
-  componentDidMount() {
-    this.setState({ isLoading: true });
-    fetch('https://swapi.co/api/people')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch.');
-        }
-        return response.json();
-      })
-      .then(charData => {
-        const selectedCharacters = charData.results.slice(0, 5);
-        this.setState({
-          characters: selectedCharacters.map((char, index) => ({
-            name: char.name,
-            id: index + 1
-          })),
-          isLoading: false
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://cors-anywhere.herokuapp.com/https://swapi.dev/api/people').then(res => {
+      if(!res.ok)
+        throw new Error('Failed to fetch!');
+      return res.json();
+    }).then(charData => {
+      const selectedCharacters = charData.results.slice(0,5);
+      setCharacters(selectedCharacters.map((char, index) => ({
+        name: char.name,
+        id: index + 1
+      })))
+      setIsLoading(false)
+    }).catch(err => {
+      console.log(err);
+      setIsLoading(false);
+    });
+  }, []);
+
+  let content = <p>Loading characters...</p>;
+
+  if(!isLoading && characters && characters.length > 0){
+    content = (
+      <select
+        onChange={props.onCharSelect}
+        value={props.selectedChar}
+        className={props.side}
+      >
+        {characters.map(char => (
+          <option key={char.id} value={char.id}>
+            {char.name}
+          </option>
+        ))}
+      </select>
+    );
+  } else if(isLoading && (!characters || characters.length === 0)){
+    content = <p>Could not fetch any data.</p>;
   }
-
-  render() {
-    let content = <p>Loading characters...</p>;
-
-    if (
-      !this.state.isLoading &&
-      this.state.characters &&
-      this.state.characters.length > 0
-    ) {
-      content = (
-        <select
-          onChange={this.props.onCharSelect}
-          value={this.props.selectedChar}
-          className={this.props.side}
-        >
-          {this.state.characters.map(char => (
-            <option key={char.id} value={char.id}>
-              {char.name}
-            </option>
-          ))}
-        </select>
-      );
-    } else if (
-      !this.state.isLoading &&
-      (!this.state.characters || this.state.characters.length === 0)
-    ) {
-      content = <p>Could not fetch any data.</p>;
-    }
-    return content;
-  }
-}
+  return content;
+};
 
 export default CharPicker;
